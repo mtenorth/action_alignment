c63d1beac736a3b24da282d8b571241393812e5d
tenorth <tenorth@14866988-fed8-44f9-99c9-aa22ffbcfa85>
2012-09-02 03:38:56 +0000

    added test sequences for hierarchical abstraction
    git-svn-id: http://code.in.tum.de/svn/action-alignment@67 14866988-fed8-44f9-99c9-aa22ffbcfa85

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,9 +14,11 @@ import alignmentAlgorithm.NeedlemanWunsch;
 import alignmentAlgorithm.SmithWaterman;
 
 import evaluation.ConfusionMatrix;
+import fileReader.LabelsCSVFileReader;
 import fileReader.LabelsDATFileReader;
 import ontology.Ontology;
 import sequence.ActionSequence;
+import sequenceElement.ActionElement;
 
 public class Main {
 
@@ -25,109 +27,109 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		try {
+//		try {
 
 
-			//		LabelsCSVFileReader r1 = new LabelsCSVFileReader();
-			//		LabelsCSVFileReader r2 = new LabelsCSVFileReader();
-			//		
-			//		ArrayList<ActionElement> seqLeft00 = r1.getSequence("Data/SequencesHands/lefthand-0-0-condensed.csv");
-			//		ArrayList<ActionElement> seqRight00 = r2.getSequence("Data/SequencesHands/righthand-0-0-condensed.csv");
-			//		
-			//		ActionSequence aLeft00 = new ActionSequence("lefthand-0-0", seqLeft00);
-			//		ActionSequence aRight00 = new ActionSequence("righthand-0-0", seqRight00);
-			//		
-			//		NeedlemanWunsch ndl = new NeedlemanWunsch(aLeft00, aRight00);
-			//		ndl.printMatrix();
-			//		ndl.printTraceback();
-			//		ndl.printAlignment();
-
-
-
-			String url = "http://ias.cs.tum.edu/kb/knowrob.owl";
-			Ontology ontology = new Ontology(url);
-			LabelsDATFileReader reader = new LabelsDATFileReader();
-
-
-			// read labels for making eggs
-
-			HashMap<String, ActionSequence> eggs = new LinkedHashMap<String, ActionSequence>();
-			for (String id : new String[]{"S06", "S07", "S08", "S09", "S11", "S12", "S13", 
-					"S14", "S15", "S16", "S17", "S19", "S20", "S25", "S53", "S54", "S55"}) {
-				eggs.put("Egg_"+id, new ActionSequence("Egg_"+id, 
-						reader.getSequence("Data/MakingEggs/"+id+"/labels.dat")));
-			}
-
-
-			// read labels for making brownies
-			HashMap<String, ActionSequence> brownies = new LinkedHashMap<String, ActionSequence>();
-			for (String id : new String[]{"S06", "S07", "S08", "S09", "S10", "S12", "S13",
-					"S14", "S16", "S17", "S18", "S19", "S20", "S22", "S23", "S24"}) {
-				brownies.put("Bro_"+id, new ActionSequence("Bro_"+id, 
-						reader.getSequence("Data/MakingBrownie/"+id+"/labels.dat")));
-			}
-
-
-			// // // // // // // // // // // // // // // // // // // // // // 
-			// GLOBAL ALIGNMENT		
-
-			String outpath = "results/weights_"+AlignmentAlgorithm.weight_act+"_"+
-												AlignmentAlgorithm.weight_obj1+"_"+
-												AlignmentAlgorithm.weight_prep+"_"+
-												AlignmentAlgorithm.weight_obj2+"/";
-
-			// comparison without WUP
-			computeAndPrintGlobalAlignment(brownies, null, outpath + "pairwise-global-alignment-brownies-nowup");
-			computeAndPrintGlobalAlignment(eggs, null, outpath + "pairwise-global-alignment-eggs-nowup");
-
-			// comparison with WUP
-			computeAndPrintGlobalAlignment(brownies, ontology, outpath + "pairwise-global-alignment-brownies-wup");
-			computeAndPrintGlobalAlignment(eggs, ontology, outpath + "pairwise-global-alignment-eggs-wup");
-
-
-			// // // // // // // // // // // // // // // // // // // // // // 
-			// LOCAL ALIGNMENT
-
-			// comparison without WUP
-			computeAndPrintLocalAlignment(brownies, null, outpath + "pairwise-local-alignment-brownies-nowup");
-			computeAndPrintLocalAlignment(eggs, null, outpath + "pairwise-local-alignment-eggs-nowup");
-
-			// comparison with WUP
-			computeAndPrintLocalAlignment(brownies, ontology, outpath + "pairwise-local-alignment-brownies-wup");
-			computeAndPrintLocalAlignment(eggs, ontology, outpath + "pairwise-local-alignment-eggs-wup");
-
-
-			// // // // // // // // // // // // // // // // // // // // // // 
-			// SIMILARITY MATRIX EGGS/BROWNIES
-
-			Writer out = new OutputStreamWriter(new FileOutputStream(new File(outpath + "confusion-matrix-nowup")));
+			LabelsCSVFileReader r1 = new LabelsCSVFileReader();
+			LabelsCSVFileReader r2 = new LabelsCSVFileReader();
 			
-			ArrayList<ActionSequence> aList = new ArrayList<ActionSequence>();
-			aList.addAll(brownies.values());
-			aList.addAll(eggs.values());
+			ArrayList<ActionElement> seqLeft00 = r1.getSequence("Data/TumKitchenData/righthand-0-11-condensed.csv");
+			ArrayList<ActionElement> seqRight00 = r2.getSequence("Data/TumKitchenData/righthand-0-7-condensed-abstracted.csv");
 			
-			out.write("\n\n\n\n\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
-			out.write("SIMILARITY MATRIX WITHOUT WUP\n");
-			out.close();
+			ActionSequence aLeft00 = new ActionSequence("lefthand-0-0", seqLeft00);
+			ActionSequence aRight00 = new ActionSequence("righthand-0-0", seqRight00);
 			
+			NeedlemanWunsch ndl = new NeedlemanWunsch(aLeft00, aRight00);
+//			ndl.printMatrix();
+//			ndl.printTraceback();
+			ndl.printAlignment();
+
+
+//
+//			String url = "http://ias.cs.tum.edu/kb/knowrob.owl";
+//			Ontology ontology = new Ontology(url);
+//			LabelsDATFileReader reader = new LabelsDATFileReader();
+//
+//
+//			// read labels for making eggs
+//
+//			HashMap<String, ActionSequence> eggs = new LinkedHashMap<String, ActionSequence>();
+//			for (String id : new String[]{"S06", "S07", "S08", "S09", "S11", "S12", "S13", 
+//					"S14", "S15", "S16", "S17", "S19", "S20", "S25", "S53", "S54", "S55"}) {
+//				eggs.put("Egg_"+id, new ActionSequence("Egg_"+id, 
+//						reader.getSequence("Data/MakingEggs/"+id+"/labels.dat")));
+//			}
+//
+//
+//			// read labels for making brownies
+//			HashMap<String, ActionSequence> brownies = new LinkedHashMap<String, ActionSequence>();
+//			for (String id : new String[]{"S06", "S07", "S08", "S09", "S10", "S12", "S13",
+//					"S14", "S16", "S17", "S18", "S19", "S20", "S22", "S23", "S24"}) {
+//				brownies.put("Bro_"+id, new ActionSequence("Bro_"+id, 
+//						reader.getSequence("Data/MakingBrownie/"+id+"/labels.dat")));
+//			}
+//
+//
+//			// // // // // // // // // // // // // // // // // // // // // // 
+//			// GLOBAL ALIGNMENT		
+//
+//			String outpath = "results/weights_"+AlignmentAlgorithm.weight_act+"_"+
+//												AlignmentAlgorithm.weight_obj1+"_"+
+//												AlignmentAlgorithm.weight_prep+"_"+
+//												AlignmentAlgorithm.weight_obj2+"/";
+//
+//			// comparison without WUP
+//			computeAndPrintGlobalAlignment(brownies, null, outpath + "pairwise-global-alignment-brownies-nowup");
+//			computeAndPrintGlobalAlignment(eggs, null, outpath + "pairwise-global-alignment-eggs-nowup");
+//
+//			// comparison with WUP
+//			computeAndPrintGlobalAlignment(brownies, ontology, outpath + "pairwise-global-alignment-brownies-wup");
+//			computeAndPrintGlobalAlignment(eggs, ontology, outpath + "pairwise-global-alignment-eggs-wup");
+//
+//
+//			// // // // // // // // // // // // // // // // // // // // // // 
+//			// LOCAL ALIGNMENT
+//
+//			// comparison without WUP
+//			computeAndPrintLocalAlignment(brownies, null, outpath + "pairwise-local-alignment-brownies-nowup");
+//			computeAndPrintLocalAlignment(eggs, null, outpath + "pairwise-local-alignment-eggs-nowup");
+//
+//			// comparison with WUP
+//			computeAndPrintLocalAlignment(brownies, ontology, outpath + "pairwise-local-alignment-brownies-wup");
+//			computeAndPrintLocalAlignment(eggs, ontology, outpath + "pairwise-local-alignment-eggs-wup");
+//
+//
+//			// // // // // // // // // // // // // // // // // // // // // // 
+//			// SIMILARITY MATRIX EGGS/BROWNIES
+//
+//			Writer out = new OutputStreamWriter(new FileOutputStream(new File(outpath + "confusion-matrix-nowup")));
+//			
+//			ArrayList<ActionSequence> aList = new ArrayList<ActionSequence>();
+//			aList.addAll(brownies.values());
+//			aList.addAll(eggs.values());
+//			
+//			out.write("\n\n\n\n\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
+//			out.write("SIMILARITY MATRIX WITHOUT WUP\n");
+//
+//			ConfusionMatrix cm_nowup = new ConfusionMatrix(aList);
+//			out.write(cm_nowup.printConfusionMatrix());
+//			out.close();
+//			
+//			
+//			out = new OutputStreamWriter(new FileOutputStream(new File(outpath + "confusion-matrix-wup")));
+//			
+//			out.write("\n\n\n\n\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
+//			out.write("SIMILARITY MATRIX WITH WUP\n");
+//			
+//			ConfusionMatrix cm_wup = new ConfusionMatrix(aList, ontology);
+//			out.write(cm_wup.printConfusionMatrix());
+//			out.close();
+//			
 			
-			out = new OutputStreamWriter(new FileOutputStream(new File(outpath + "confusion-matrix-wup")));
-			
-			ConfusionMatrix cm_nowup = new ConfusionMatrix(aList);
-			out.write(cm_nowup.printConfusionMatrix());
-	
-			out.write("\n\n\n\n\n\n= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =\n");
-			out.write("SIMILARITY MATRIX WITH WUP\n");
-			
-			ConfusionMatrix cm_wup = new ConfusionMatrix(aList, ontology);
-			out.write(cm_wup.printConfusionMatrix());
-			out.close();
-			
-			
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 
